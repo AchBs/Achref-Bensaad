bbbe597ca21a8828d33684ab5eca7ba590087e11
Achref <achref.bensaad@isitc.u-sousse.tn>
2024-01-11 14:30:30 +0100

    changes made

--- javascript/adopt.js
+++ javascript/adopt.js
@@ -9,3 +9,23 @@ function openAdoptionForm(dogName) {
     const adoptionFormUrl = `adoption_form.html?dogName=${encodeURIComponent(dogName)}`;
     window.open(adoptionFormUrl, '_blank', `width=${windowWidth}, height=${windowHeight}, left=${windowLeft}, top=${windowTop}`);
 }
+
+
+function playDogAudio() {
+    var audio = new Audio(' ../audio/Dog_audio.mp3'); // Replace 'path/to/dog_audio.mp3' with the actual path to your dog audio file
+    audio.play();
+}
+
+function playCatAudio() {
+    var audio = new Audio('../audio/Cat_audio.mp3'); // Replace 'path/to/cat_audio.mp3' with the actual path to your cat audio file
+    audio.play();
+}
+
+function playHamsterAudio() {
+    var audio = new Audio('../audio/hamster_audio.mp3'); // Replace 'path/to/hamster_audio.mp3' with the actual path to your hamster audio file
+    audio.play();
+}
+function playHorseAudio() {
+    var audio = new Audio('../audio/horse_audio.mp3'); // Replace 'path/to/hamster_audio.mp3' with the actual path to your hamster audio file
+    audio.play();
+}
